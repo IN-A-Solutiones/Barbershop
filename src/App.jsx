ba49cd294b864a9819150eca55e1577375647aab
Tamas <tamas.fehertoi@gmail.com>
2024-12-19 17:08:57 -0500

    Our story content and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Landing from "./pages/Langing/Landing";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,48 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./Landing.css";
+import { SlArrowDown } from "react-icons/sl";
+import AboutImage from "../../assets/Images/about.webp";
+
+import React from "react";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <Navbar />
+      </nav>
+      <header>
+        <p>Gentlemen's</p>
+        <h1>Barbershop</h1>
+        <SlArrowDown size={80} className="arrow-down" />
+      </header>
+      <section className="about" id="about">
+        <div className="about-container">
+          <div className="about-image">
+            <img src={AboutImage} alt="About us" />
+          </div>
+          <div className="about-content">
+            <h2>Történetünk</h2>
+            <p>
+              Szenvedélyünk a férfiak ápoltsága, és hiszünk abban, hogy egy jó
+              hajvágás vagy borotválkozás nem csupán rutin, hanem igazi rituálé.
+              Célunk, hogy minden vendégünk számára egyedi élményt nyújtsunk –
+              egy helyet, ahol kikapcsolódhatsz, feltöltődhetsz, és magabiztosan
+              távozhatsz.
+            </p>
+            <p>
+              Történetünk a szakmánk iránti elkötelezettségről és a közösség
+              iránti tiszteletről szól. Minden ollóvágásban, minden sima
+              borotvahúzásban ott van a múlt mestereinek hagyatéka, amit a
+              modern férfi igényeivel ötvözünk.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <footer></footer>
+    </main>
+  );
+};
+
+export default Landing;
